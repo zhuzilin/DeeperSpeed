e4689e2ebc7483400464b2982e30528633f371e3
sdtblck <46172032+sdtblck@users.noreply.github.com>
2021-05-02 12:44:55 +0200

    Update __init__.py

--- deepspeed/ops/sparse_attention/__init__.py
+++ deepspeed/ops/sparse_attention/__init__.py
@@ -1,4 +1,4 @@
-from .sparsity_config import SparsityConfig, DenseSparsityConfig, FixedSparsityConfig, VariableSparsityConfig, BigBirdSparsityConfig, BSLongformerSparsityConfig
+from .sparsity_config import SparsityConfig, DenseSparsityConfig, FixedSparsityConfig, VariableSparsityConfig, BigBirdSparsityConfig, BSLongformerSparsityConfig, LocalSlidingWindowSparsityConfig
 from .softmax import Softmax
 from .matmul import MatMul
 from .sparse_self_attention import SparseSelfAttention
